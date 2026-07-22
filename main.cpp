1b779f61e5a995efe1b2c4a16b8a915da037e6b5
TonyKaatib <95652181+TonyKaatib@users.noreply.github.com>
2026-07-22 22:45:56 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <graphics.h>
+#include <cstdio>
+#include <conio.h>
+
+static int program = 1;
+
+void presentframe(int& page) {
+    setvisualpage(page);
+    page = 1 - page;
+    setactivepage(page);
+}
+
+int main(int argc, char** argv){
+	initwindow(800, 800, "Title of the window");
+	
+	int page = 0;
+	int x = 10;
+	int y = 100;
+	int width = 90;
+	int height = 100;
+	int dx = 3;
+	int dy = 3;
+	char exitText[] = "Press any key to exit";
+	
+	while (program == 1){
+		if (kbhit()) {
+			program = 0;
+		}
+
+		x += dx;
+		y += dy;
+
+		if (x <= 0 || x + width >= getmaxx()) {
+			dx = -dx;
+			x += dx;
+		}
+
+		if (y <= 0 || y + height >= getmaxy()) {
+			dy = -dy;
+			y += dy;
+		}
+
+		cleardevice();
+		setbkcolor(BLACK);
+		setcolor(WHITE);
+		settextjustify(CENTER_TEXT, TOP_TEXT);
+		rectangle(x, y, x + width, y + height);
+		outtextxy(getmaxx() / 2, getmaxy() / 2, exitText);
+		presentframe(page);
+		
+		delay(16);
+	};
+	cleardevice();
+	return 0;
+}
